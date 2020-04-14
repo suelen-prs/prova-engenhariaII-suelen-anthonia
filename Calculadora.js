56d1d082597e5f63a58d9d8a2affa6a49f118851
suelen-prs <suelen.prs@gmail.com>
2020-04-13 22:47:30 -0300

    :art: adicionando a função subtracao
    desenvolvimento do codico da funcao de subtracao

--- Calculadora.js
+++ Calculadora.js
@@ -2,8 +2,8 @@ function sum(num1, num2){
     return num1 + num2
 }
 
-function sub(){
-    //TODO: Implementar subtração
+function sub(num1, num2){
+    return num1 - num2
 }
 
 function mult(num1, num2){
@@ -11,15 +11,10 @@ function mult(num1, num2){
 }
 
 function div(num1,num2){
-    return num1 / num 2
+    return num1 / num2
 }
-<<<<<<< HEAD
 
 console.log(mult(2,5))
 
 
 console.log(sum(20, 60))
-=======
-console.log(mult(2,5))
-console.log(div(50,2))
->>>>>>> d77fe44dcc07cb9e7e36eb7f3d982e4c13d3a852
