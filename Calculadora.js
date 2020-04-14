0901e9e3c6c79f51ee2dc6952db7211af6743ddb
suelen-prs <suelen.prs@gmail.com>
2020-04-13 23:01:07 -0300

    :zap: atualizando funcao soma

--- Calculadora.js
+++ Calculadora.js
@@ -1,5 +1,5 @@
-function sum(num1, num2){
-    return num1 + num2
+function sum(num, num1){
+    return num + num2
 }
 
 function sub(){
@@ -13,13 +13,8 @@ function mult(num1, num2){
 function div(num1,num2){
     return num1 / num 2
 }
-<<<<<<< HEAD
 
 console.log(mult(2,5))
 
 
 console.log(sum(20, 60))
-=======
-console.log(mult(2,5))
-console.log(div(50,2))
->>>>>>> d77fe44dcc07cb9e7e36eb7f3d982e4c13d3a852
