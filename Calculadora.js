c05549ca1f073603d559b402b44d54290316090a
AnthoniaS <anthoniasr@gmail.com>
2020-04-13 21:17:00 -0300

    :sparkles: Adiciona função de multiplicação

--- Calculadora.js
+++ Calculadora.js
@@ -6,10 +6,11 @@ function sub(){
     //TODO: Implementar subtração
 }
 
-function mult(){
-    //TODO: Implementar multiplicação
+function mult(num1, num2){
+    return num1 * num
 }
 
 function div(){
     //TODO: Implementar divisão
 }
+console.log(mult(2,5))
